b0f750a951c84d52006ad181ad9cb5120b0ddd20
Gustavo Farias <gustavo.silva@ccc.ufcg.edu.br>
2024-04-20 11:29:01 -0300

    Update form page

--- src/pages/visitation/form/index.tsx
+++ src/pages/visitation/form/index.tsx
@@ -8,10 +8,11 @@ import { AuthContext } from "@/src/contexts/AuthContext";
 
 import { Header } from "@/src/components/Header";
 import { BackButton, CanceltButton, EditButton, SaveButton } from "@/src/components/ui/Button";
-import { VisitationStatus } from "@/src/components/VisitationStatus";
-import { DepositoSection } from "@/src/components/DepositoSection";
-import { HeaderSection } from "@/src/components/HeaderSection";
-import { AmostraSection } from "@/src/components/AmostraSection";
+import { VisitationStatus } from "@/src/components/Section/VisitationStatus";
+import { DepositoSection } from "@/src/components/Section/DepositoSection";
+import { HeaderSection } from "@/src/components/Section/HeaderSection";
+import { AmostraSection } from "@/src/components/Section/AmostraSection";
+import { TratamentoSection } from "@/src/components/Section/TratamentoSection";
 
 
 export default function Visitation({ visitation_area_id, visitation_id, visitation }) {
@@ -30,16 +31,18 @@ export default function Visitation({ visitation_area_id, visitation_id, visitati
     deposito: {
       a1: visitation?.deposito?.a1 || 0, a2: visitation?.deposito?.a2 || 0,
       b: visitation?.deposito?.b || 0, c: visitation?.deposito?.c || 0,
-      d1: visitation?.deposito?.d1 || 0, d2: visitation?.deposito?.d2 || 0,
-      e: visitation?.deposito?.e || 0, eliminado: visitation?.deposito?.eliminado || 0,
-      inspecionados: visitation?.deposito?.inspecionados || 0
+      d1: visitation?.deposito?.d1 || 0, d2: visitation?.deposito?.d2 || 0, e: visitation?.deposito?.e || 0, 
+      eliminado: visitation?.deposito?.eliminado || 0, inspecionados: visitation?.deposito?.inspecionados || 0
     },
     amostra: {
-      inicial: visitation?.amostra?.inicial || "", final: visitation?.amostra?.final || "",
-      tubitos: visitation?.amostra?.final || ""
+      inicial: visitation?.amostra?.inicial || "", final: visitation?.amostra?.final || "", tubitos: visitation?.amostra?.tubitos || 0
     },
     tratamento: {
-      "value": "value"
+      qtd_dep_elimind: visitation?.tratamento?.qtd_dep_elimind || 0, imoveis_tratd: visitation?.tratamento?.imoveis_tratd || 0,
+      tipo_larvcd1: visitation?.tratamento?.tipo_larvcd1 || "", tipo_larvcd2: visitation?.tratamento?.tipo_larvcd2 || "",
+      qtd_grms_larvcd1: visitation?.tratamento?.qtd_grms_larvcd1 || 0, qtd_grms_larvcd2: visitation?.tratamento?.qtd_grms_larvcd2 || 0,
+      qtd_dep_trat_larvcd1: visitation?.tratamento?.qtd_dep_trat_larvcd1 || 0, qtd_dep_trat_larvcd2: visitation?.tratamento?.qtd_dep_trat_larvcd2 || 0,
+      tipo_adultcd: visitation?.tratamento?.tipo_adultcd || "", qtd_crgs_adultcd: visitation?.tratamento?.qtd_crgs_adultcd || 0
     }
   });
 
@@ -146,6 +149,12 @@ export default function Visitation({ visitation_area_id, visitation_id, visitati
             />
 
             <AmostraSection
+              handleInputChange={handleInputChange}
+              isEditing={isEditing}
+              formData={formData}
+            />
+
+            <TratamentoSection
               handleInputChange={handleInputChange}
               isEditing={isEditing}
               formData={formData}
